64b5fa6229bac9fa7a1500807f46151e05aa297b
Jeongmin <skelters@naver.com>
2025-07-26 23:01:02 +0900

    이미 폴더가 생성되어있으면 Reinitaial 콘솔을, 아니면 mygit init을 출력

--- src/init.ts
+++ src/init.ts
@@ -1,11 +1,17 @@
 import { mkdir } from "fs/promises";
 import * as fs from "node:fs";
+import * as path from "path";
 
 export async function initGit() {
-  try {
-    await mkdir("./.mygit", { recursive: true });
-    console.log(".git dir created");
-  } catch (err) {
-    console.error(err);
+  const pwd = path.join(process.cwd(), ".mygit");
+  if (fs.existsSync("./mygit")) {
+    console.log(`Reinitailized existing repository in ${pwd}`);
+  } else {
+    try {
+      await mkdir("./.mygit", { recursive: true });
+      console.log("Initialized");
+    } catch (err) {
+      console.error("Something went wrong when create .mygit dir!", err);
+    }
   }
 }
